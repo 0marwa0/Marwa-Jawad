eea5fdb35db8afefc1754ca7cab908d11fd529c9
marwa <marwajawad19@gmail.com>
2022-04-01 09:37:56 +0300

    add eslint ignore

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,44 +1,42 @@
-import React from "react";
-import { connect } from "react-redux";
-import { fetchCategory } from "../../store/categoriesSlice";
-import { fetchCurrency } from "../../store/currencySlice";
-import ProductCard from "../../components/product/card";
-import Pagination from "../../components/pagination";
-import "./index.css";
+import React from 'react'
+import { connect } from 'react-redux'
+import { fetchCategory } from '../../store/categoriesSlice'
+import { fetchCurrency } from '../../store/currencySlice'
+import ProductCard from '../../components/product/card'
+import Pagination from '../../components/pagination'
+import './index.css'
 class Products extends React.Component {
-  state = { currentPage: 1, perPage: 6 };
+  state = { currentPage: 1, perPage: 6 }
   componentDidMount() {
-    this.props.getCategories();
-    this.props.getCurrency();
+    this.props.getCategories()
+    this.props.getCurrency()
   }
   nextPage = () => {
-    const totalPage = Math.ceil(
-      this.props.products.length / this.state.perPage
-    );
+    const totalPage = Math.ceil(this.props.products.length / this.state.perPage)
     if (totalPage !== this.state.currentPage) {
       this.setState(() => ({
         currentPage: this.state.currentPage + 1,
-      }));
+      }))
     }
-  };
+  }
 
   prevPage = () => {
     if (this.state.currentPage > 1)
       this.setState(() => ({
         currentPage: this.state.currentPage - 1,
-      }));
-  };
+      }))
+  }
   componentWillReceiveProps(nextProps) {
     if (nextProps.products !== this.props.products) {
-      this.setState({ currentPage: 1 });
+      this.setState({ currentPage: 1 })
     }
   }
   render() {
-    let products = this.props.products || [];
-    let lastIndex = this.state.currentPage * this.state.perPage;
-    let firstIndex = lastIndex - this.state.perPage;
-    products = products?.slice(firstIndex, lastIndex);
-    let total = Math.ceil(this.props.products?.length / this.state.perPage);
+    let products = this.props.products || []
+    let lastIndex = this.state.currentPage * this.state.perPage
+    let firstIndex = lastIndex - this.state.perPage
+    products = products?.slice(firstIndex, lastIndex)
+    let total = Math.ceil(this.props.products?.length / this.state.perPage)
     return (
       <div>
         <h1 className="product-title">Category Name </h1>
@@ -59,7 +57,7 @@ class Products extends React.Component {
           ))}
         </div>
       </div>
-    );
+    )
   }
 }
 const state = function (state) {
@@ -69,14 +67,14 @@ const state = function (state) {
         ? state.categories.currentCategory.products
         : state.categories.categories[0]?.products,
     currencies: state.currencies,
-  };
-};
+  }
+}
 
 const dispatch = (dispatch) => {
   return {
     getCategories: () => dispatch(fetchCategory()),
     getCurrency: () => dispatch(fetchCurrency()),
-  };
-};
+  }
+}
 
-export default connect(state, dispatch)(Products);
+export default connect(state, dispatch)(Products)
