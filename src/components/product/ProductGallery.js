f1af18feba42ff5e93c8be908616d600392af3bc
marwa <marwajawad19@gmail.com>
2022-04-08 08:55:22 +0300

    make image thumbnails on switch the big image

--- src/components/product/ProductGallery.js
+++ src/components/product/ProductGallery.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import LazyLoading from '../LazyLoading'
 class index extends React.Component {
+  state = { mainImage: this.props.gallery }
+  onDisplay = (src) => {
+    this.setState(() => ({ mainImage: src }))
+  }
+
   render() {
     const gallery = this.props.gallery
     return (
       <div className="product-gallery">
         <div className="product-images">
           {gallery?.map((src, i) => (
-            <div key={i}>
+            <div key={i} onClick={() => this.onDisplay(src)}>
               <LazyLoading src={src} item={this.props.item} />
             </div>
           ))}
         </div>
 
         <div className="main-image">
-          <LazyLoading src={gallery?.[0]} item={this.props.item} />
+          <LazyLoading
+            src={this.state.mainImage ?? gallery?.[0]}
+            item={this.props.item}
+          />
         </div>
       </div>
     )
